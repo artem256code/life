b67d151eb6171c35735c01afec6ba02e48b2735a
artem256code <artem_code@outlook.com>
2020-11-10 16:20:10 +0300

    Added an empty template for a processing the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "game.hpp"
 
 int main(){
-    
+    Game game("Life", 400, 250);    
+
+    while(true){
+        //Clean scene                   Game.clear();
+        //Check input                   Game.input(); 
+        //Process the all game logic    Game.update();
+        //Render scene                  Game.render();
+    }
 
     return 0;
 }
