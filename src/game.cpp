85e5627fb9d12352152a37273c5c26832321e0d2
artem256code <artem_code@outlook.com>
2020-11-10 16:23:23 +0300

    Added the consturctor fot the 'Game'

--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,27 @@
 #include "game.hpp"
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+
+Game::Game(std::string title,short with, short height){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        std::cout << "Failed init SDL lib...";
+    }
+    window = SDL_CreateWindow(title.c_str(), 
+                              SDL_WINDOWPOS_UNDEFINED, 
+                              SDL_WINDOWPOS_UNDEFINED, 
+                              400, 
+                              500, 
+                              SDL_WINDOW_OPENGL);
+    if(window == nullptr){
+        std::cout << "Failed create SDL_Window...";
+    }
+
+    renderer = SDL_CreateRenderer(window, 
+                                  -1, 
+                                  SDL_RENDERER_ACCELERATED);
+    if(renderer == nullptr){
+        std::cout << "Failed create SDL_Renderer";
+    }
+}
